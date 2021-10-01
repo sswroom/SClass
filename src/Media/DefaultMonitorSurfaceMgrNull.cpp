4b20616f8591ccbe748cb96824713777a03ed462
Simon Wong <sswroom@yahoo.com>
2021-10-01 12:27:34 +0800

    Added DefaultMonitorSurfaceMgrNull

---
+++ src/Media/DefaultMonitorSurfaceMgrNull.cpp
@@ -0,0 +1,12 @@
+#include "Stdafx.h"
+#include "Media/DefaultMonitorSurfaceMgr.h"
+
+Media::MonitorSurfaceMgr *Media::DefaultMonitorSurfaceMgr::Create(Media::MonitorMgr *monMgr, Media::ColorManagerSess *colorSess)
+{
+	return 0;
+}
+
+Media::MonitorSurfaceMgr *Media::DefaultMonitorSurfaceMgr::Create(Media::MonitorMgr *monMgr, Media::ColorManager *colorMgr)
+{
+	return 0;
+}
