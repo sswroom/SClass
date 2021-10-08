e0e837167569e57ed0d2cf8d409af4db2fa7c3bb
Simon Wong <sswroom@yahoo.com>
2021-10-08 20:24:43 +0800

    Added UnicodeBlock

--- header/Text/CharUtil.h
+++ header/Text/CharUtil.h
@@ -6,12 +6,25 @@ namespace Text
 	class CharUtil
 	{
 	public:
+		struct UnicodeBlock
+		{
+			UInt32 firstCode;
+			UInt32 lastCode;
+			Bool dblWidth;
+			const Char *name;
+		};
+
+	private:
+		static UnicodeBlock ublk[];
+	public:
 		static Bool PtrIsWS(const UTF8Char **sptr); //Point to next char if true
 		static Bool IsWS(const UTF8Char *sptr);
 		static Bool IsPunctuation(UTF8Char c);
 		static Bool IsAlphaNumeric(UTF8Char c);
 		static Bool IsAlphabet(UTF8Char c);
 		static Bool UTF8CharValid(const UTF8Char *sptr);
+		static const UnicodeBlock *GetUnicodeBlock(UTF32Char c);
+		static Bool IsDoubleSize(UTF32Char c);
 	};
 }
 #endif
