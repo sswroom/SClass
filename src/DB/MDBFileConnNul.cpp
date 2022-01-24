468eec8fe3a356b68088fc2c2677310f1e12107b
Simon Wong <sswroom@yahoo.com>
2022-01-24 14:07:24 +0800

    Fixed android compiling

--- src/DB/MDBFileConnNul.cpp
+++ src/DB/MDBFileConnNul.cpp
@@ -1,26 +1,26 @@
 #include "Stdafx.h"
 #include "DB/DBTool.h"
-#include "DB/MDBFile.h"
+#include "DB/MDBFileConn.h"
 #include "Text/Encoding.h"
 #include "Text/EncodingFactory.h"
 #include "Text/Locale.h"
 
 
-DB::MDBFile::MDBFile(const UTF8Char *fileName, IO::LogTool *log, UInt32 codePage, const WChar *uid, const WChar *pwd) : DB::ODBCConn(fileName, log)
+DB::MDBFileConn::MDBFileConn(const UTF8Char *fileName, IO::LogTool *log, UInt32 codePage, const WChar *uid, const WChar *pwd) : DB::ODBCConn(fileName, log)
 {
 }
 
-Bool DB::MDBFile::CreateMDBFile(const UTF8Char *fileName)
+Bool DB::MDBFileConn::CreateMDBFile(const UTF8Char *fileName)
 {
 	return false;
 }
 
-DB::DBTool *DB::MDBFile::CreateDBTool(Text::String *fileName, IO::LogTool *log, const UTF8Char *logPrefix)
+DB::DBTool *DB::MDBFileConn::CreateDBTool(Text::String *fileName, IO::LogTool *log, const UTF8Char *logPrefix)
 {
 	return 0;
 }
 
-DB::DBTool *DB::MDBFile::CreateDBTool(const UTF8Char *fileName, IO::LogTool *log, const UTF8Char *logPrefix)
+DB::DBTool *DB::MDBFileConn::CreateDBTool(const UTF8Char *fileName, IO::LogTool *log, const UTF8Char *logPrefix)
 {
 	return 0;
 }
