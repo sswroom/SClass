b37421b81c4aa6600936cac1b71aba87474cc5cb
Simon Wong <sswroom@yahoo.com>
2023-10-27 01:08:17 +0800

    Enhance WKBWriter

--- src/Math/WKBWriter.cpp
+++ src/Math/WKBWriter.cpp
@@ -1,6 +1,8 @@
 #include "Stdafx.h"
 #include "Math/WKBWriter.h"
 
+#include <stdio.h>
+
 Math::WKBWriter::WKBWriter()
 {
 }
@@ -11,5 +13,6 @@ Math::WKBWriter::~WKBWriter()
 
 Bool Math::WKBWriter::Write(NotNullPtr<IO::Stream> stm, NotNullPtr<Math::Geometry::Vector2D> vec)
 {
+	printf("WKBWriter: Unsupported type: %s\r\n", Math::Geometry::Vector2D::VectorTypeGetName(vec->GetVectorType()).v);
 	return false;
 }
