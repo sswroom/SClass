962b6901fcb6513d36c5892c1650ff8dc2eb745b
Simon Wong <sswroom@yahoo.com>
2023-11-13 06:01:31 +0800

    Fixed missing service

--- src/SSWR/OrganWeb/OrganWebPOIController.cpp
+++ src/SSWR/OrganWeb/OrganWebPOIController.cpp
@@ -1413,6 +1413,8 @@ SSWR::OrganWeb::OrganWebPOIController::OrganWebPOIController(Net::WebServer::Mem
 	this->AddService(CSTR("/api/bookdetail"), Net::WebUtil::RequestMethod::HTTP_GET, SvcBookDetail);
 	this->AddService(CSTR("/api/photodetail"), Net::WebUtil::RequestMethod::HTTP_POST, SvcPhotoDetail);
 	this->AddService(CSTR("/api/photoupload"), Net::WebUtil::RequestMethod::HTTP_POST, SvcPhotoUpload);
+	this->AddService(CSTR("/api/photoname"), Net::WebUtil::RequestMethod::HTTP_POST, SvcPhotoName);
+	this->AddService(CSTR("/api/unfinpeak"), Net::WebUtil::RequestMethod::HTTP_POST, SvcUnfinPeak);
 	this->AddService(CSTR("/api/reload"), Net::WebUtil::RequestMethod::HTTP_POST, SvcReload);
 	this->AddService(CSTR("/api/publicpoi"), Net::WebUtil::RequestMethod::HTTP_GET, SvcPublicPOI);
 	this->AddService(CSTR("/api/grouppoi"), Net::WebUtil::RequestMethod::HTTP_GET, SvcGroupPOI);
