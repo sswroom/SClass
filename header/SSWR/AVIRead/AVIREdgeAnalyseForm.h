8c994203c7bc2706b11ef57c2bdcad5cfb7c0f99
Simon Wong <sswroom@yahoo.com>
2025-06-11 02:19:38 +0800

    Enhance Edge Analyse

--- header/SSWR/AVIRead/AVIREdgeAnalyseForm.h
+++ header/SSWR/AVIRead/AVIREdgeAnalyseForm.h
@@ -2,8 +2,11 @@
 #define _SM_SSWR_AVIREAD_AVIREDGEANALYSEFORM
 #include "SSWR/AVIRead/AVIRCore.h"
 #include "UI/GUIButton.h"
+#include "UI/GUILabel.h"
+#include "UI/GUIListView.h"
 #include "UI/GUIPanel.h"
 #include "UI/GUITabControl.h"
+#include "UI/GUITabPage.h"
 #include "UI/GUITextBox.h"
 
 namespace SSWR
@@ -17,16 +20,21 @@ namespace SSWR
 			NN<UI::GUITabControl> tcMain;
 
 			NN<UI::GUITabPage> tpWebHook;
-			NN<UI::GUIPanel> pnlCtrl;
-			NN<UI::GUIButton> btnSave;
-			NN<UI::GUIButton> btnHex;
-			NN<UI::GUITextBox> txtEDID;
+			NN<UI::GUIPanel> pnlWebHook;
+			NN<UI::GUILabel> lblWebHookPort;
+			NN<UI::GUITextBox> txtWebHookPort;
+			NN<UI::GUIButton> btnWebHook;
+			NN<UI::GUITextBox> txtWebHookData;
+			NN<UI::GUIListView> lvWebHookData;
 			
+			static void __stdcall OnWebHookClicked(AnyType userObj);
+			static void __stdcall OnWebHookDataSelChg(AnyType userObj);
 		public:
 			AVIREdgeAnalyseForm(Optional<UI::GUIClientControl> parent, NN<UI::GUICore> ui, NN<SSWR::AVIRead::AVIRCore> core);
 			virtual ~AVIREdgeAnalyseForm();
 
 			virtual void OnMonitorChanged();
+			void AddRecord(Data::Timestamp ts, NN<Text::String> type, NN<Text::JSONBase> json);
 		};
 	}
 }
--- src/SSWR/AVIRead/AVIREdgeAnalyseForm.cpp
+++ src/SSWR/AVIRead/AVIREdgeAnalyseForm.cpp
@@ -1,6 +1,18 @@
 #include "Stdafx.h"
 #include "SSWR/AVIRead/AVIREdgeAnalyseForm.h"
 
+void __stdcall SSWR::AVIRead::AVIREdgeAnalyseForm::OnWebHookClicked(AnyType userObj)
+{
+	NN<SSWR::AVIRead::AVIREdgeAnalyseForm> me = userObj.GetNN<SSWR::AVIRead::AVIREdgeAnalyseForm>();
+
+}
+
+void __stdcall SSWR::AVIRead::AVIREdgeAnalyseForm::OnWebHookDataSelChg(AnyType userObj)
+{
+	NN<SSWR::AVIRead::AVIREdgeAnalyseForm> me = userObj.GetNN<SSWR::AVIRead::AVIREdgeAnalyseForm>();
+
+}
+
 SSWR::AVIRead::AVIREdgeAnalyseForm::AVIREdgeAnalyseForm(Optional<UI::GUIClientControl> parent, NN<UI::GUICore> ui, NN<SSWR::AVIRead::AVIRCore> core) : UI::GUIForm(parent, 1024, 768, ui)
 {
 	this->core = core;
@@ -12,6 +24,27 @@ SSWR::AVIRead::AVIREdgeAnalyseForm::AVIREdgeAnalyseForm(Optional<UI::GUIClientCo
 	this->tcMain->SetDockType(UI::GUIControl::DOCK_FILL);
 
 	this->tpWebHook = this->tcMain->AddTabPage(CSTR("Web Hook"));
+	this->pnlWebHook = ui->NewPanel(this->tpWebHook);
+	this->pnlWebHook->SetRect(0, 0, 100, 55, false);
+	this->pnlWebHook->SetDockType(UI::GUIControl::DOCK_TOP);
+	this->lblWebHookPort = ui->NewLabel(this->pnlWebHook, CSTR("Port"));
+	this->lblWebHookPort->SetRect(4, 4, 100, 23, false);
+	this->txtWebHookPort = ui->NewTextBox(this->pnlWebHook, CSTR("8080"));
+	this->txtWebHookPort->SetRect(104, 4, 100, 23, false);
+	this->btnWebHook = ui->NewButton(this->pnlWebHook, CSTR("Start"));
+	this->btnWebHook->SetRect(104, 28, 75, 23, false);
+	this->btnWebHook->HandleButtonClick(OnWebHookClicked, this);
+	this->txtWebHookData = ui->NewTextBox(this->tpWebHook, CSTR(""), true);
+	this->txtWebHookData->SetRect(0, 0, 100, 120, false);
+	this->txtWebHookData->SetReadOnly(true);
+	this->lvWebHookData = ui->NewListView(this->tpWebHook, UI::ListViewStyle::Table, 3);
+	this->lvWebHookData->SetDockType(UI::GUIControl::DOCK_FILL);
+	this->lvWebHookData->SetFullRowSelect(true);
+	this->lvWebHookData->SetShowGrid(true);
+	this->lvWebHookData->AddColumn(CSTR("Time"), 120);
+	this->lvWebHookData->AddColumn(CSTR("Type"), 100);
+	this->lvWebHookData->AddColumn(CSTR("Desc"), 300);
+	this->lvWebHookData->HandleSelChg(OnWebHookDataSelChg, this);
 }
 
 SSWR::AVIRead::AVIREdgeAnalyseForm::~AVIREdgeAnalyseForm()
@@ -21,4 +54,9 @@ SSWR::AVIRead::AVIREdgeAnalyseForm::~AVIREdgeAnalyseForm()
 void SSWR::AVIRead::AVIREdgeAnalyseForm::OnMonitorChanged()
 {
 	this->SetDPI(this->core->GetMonitorHDPI(this->GetHMonitor()), this->core->GetMonitorDDPI(this->GetHMonitor()));
+}
+
+void SSWR::AVIRead::AVIREdgeAnalyseForm::AddRecord(Data::Timestamp ts, NN<Text::String> type, NN<Text::JSONBase> json)
+{
+
 }
