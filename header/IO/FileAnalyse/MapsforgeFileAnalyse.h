96dd3b775bef7864160863900a45ec39ebd0ec21
Simon Wong <sswroom@yahoo.com>
2025-07-18 20:46:00 +0800

    Enhance Mapsforge file

--- header/IO/FileAnalyse/MapsforgeFileAnalyse.h
+++ header/IO/FileAnalyse/MapsforgeFileAnalyse.h
@@ -14,20 +14,41 @@ namespace IO
 		class MapsforgeFileAnalyse : public IO::FileAnalyse::FileAnalyser
 		{
 		private:
+			enum class PackType
+			{
+				FileHeader,
+				TileIndex,
+				TileData
+			};
 			typedef struct
 			{
 				UInt64 fileOfst;
 				UOSInt packSize;
-				UInt16 packType;
+				PackType packType;
+				UInt8 baseZoomLevel;
+				UInt8 minZoomLevel;
+				UInt8 maxZoomLevel;
+				Int32 tileX;
+				Int32 tileY;
 			} PackInfo;
 		private:
 			Optional<IO::StreamData> fd;
 			Data::SyncArrayListNN<PackInfo> packs;
 
+			Double minLat;
+			Double minLon;
+			Double maxLat;
+			Double maxLon;
+			UOSInt tileSize;
+			UInt8 flags;
+			Data::ArrayListStringNN poiTags;
+			Data::ArrayListStringNN wayTags;
+
 			Bool pauseParsing;
 			Sync::Thread thread;
 			static void __stdcall ParseThread(NN<Sync::Thread> thread);
 			static UOSInt ReadVBEU(UnsafeArray<UInt8> buff, UOSInt ofst, OutParam<UInt64> v);
+			static UOSInt ReadVBES(UnsafeArray<UInt8> buff, UOSInt ofst, OutParam<Int64> v);
 		public:
 			MapsforgeFileAnalyse(NN<IO::StreamData> fd);
 			virtual ~MapsforgeFileAnalyse();
